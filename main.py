9a1a6fa7d88f55c2ea2c5618f0efe87ee205a4e9
Artem <118067319+artembeloglazov164@users.noreply.github.com>
2022-11-13 22:05:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-text_input = input("введи чё нибудь:")
+text_input = input("введите что искать:")
 for i in range(1,4):
     driver.get(f"https://www.avito.ru/saratov?p={i}&q={text_input}")
     time.sleep(3)
